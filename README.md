6f51cf18232356ee56531515cba219704a90bedd
yourlocalcyborg <duwareon@gmail.com>
2026-02-25 15:49:18 -0500

    fleshed out algorithm

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # CP431-TP
 
-## M(5)
+## Hand matrices
 ---
+### M(5)
 |x|-| 1|    2| 3| 4| 5|
 |-|-|--|-----|--|--|--|
 |1|-| 1|    2| 3| 4| 5|
@@ -12,12 +13,11 @@
 
 an x designates that the given value is mirrored, strikethrough designates a number as a non-trivial duplicate.
 
-$E(5) = \{1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 20, 25\}$
+$E(5) = \{ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 20, 25 \}$
 
 $M(5) = |E(5)| = 14$
 
 ## M(10)
----
 | x|-| 1| 2| 3| 4| 5| 6| 7| 8| 9| 10|
 |--|-|--|--|--|--|--|--|--|--|--|---|
 | 1|-| 1| 2| 3| 4| 5|  |  |  |  |   |
@@ -30,3 +30,7 @@ $M(5) = |E(5)| = 14$
 | 8|-|  |  |  |  |  |  |  |  |  |   |
 | 9|-|  |  |  |  |  |  |  |  |  |   |
 |10|-|  |  |  |  |  |  |  |  |  |   |
+
+## Algorithm
+
+a dispatcher ($D$) gives a worker from a set of workers with ID $n$ $(W_n)$ a column $i$, and $W_n$ calculates $A_i = \{ i*1, i*2, ... , i*i \}$. $W_n$ finishes calculation, then sends $A_i$ to $D$, and $D$ sends $W_n$ some other column $i' > i$, before adding the numbers found in $A_i$ to its complete set of found numbers.
--- main.c
+++ main.c
@@ -1,28 +1,50 @@
 #include <mpi.h>
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+int dispatcher(int size, MPI_Comm comm) {
+  printf("DISPATCHER START\n");
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Init workers
+  unsigned long long int results[] = {0xfffff};
+  for (int i = 0; i < size; i++) {
+    MPI_Send(results, 1, MPI_UNSIGNED_LONG, i, 0, comm);
+  }
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
-
-    // Finalize the MPI environment.
-    MPI_Finalize();
+  // Wait for worker finish, re-dispatch
+  while (1) {
+    //MPI_Recv();
+    break;
+  }
+  return 0;
 }
 
+int worker(int rank, MPI_Comm comm) {
+  printf("WORKER %d START\n", rank);
+  unsigned long long int results[] = {0x00};
+  MPI_Recv(results, 1, MPI_UNSIGNED_LONG_LONG, 0, 0, comm, MPI_STATUS_IGNORE);
+  printf("WORKER %d RECEIVE: %#x \n", rank, results[0]);
+  return 0;	
+}
+
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  
+  if (world_rank > 0) {
+    worker(world_rank, MPI_COMM_WORLD);
+  }
+  else {
+    dispatcher(world_size, MPI_COMM_WORLD);
+  }
+  
+  // Finalize the MPI environment.
+  MPI_Finalize();
+}
